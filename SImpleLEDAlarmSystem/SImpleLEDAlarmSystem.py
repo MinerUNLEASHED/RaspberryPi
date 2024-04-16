b34e1edcf6f09a4a22c075347efcf6730aa9ad25
MEOW CHEN <2020tray@gmail.com>
2024-04-16 16:53:53 +0100

    code from raspberry pi

--- SImpleLEDAlarmSystem/SImpleLEDAlarmSystem.py
+++ SImpleLEDAlarmSystem/SImpleLEDAlarmSystem.py
@@ -3,8 +3,11 @@ from gpiozero import DistanceSensor, LED
 open_image = open('rpi.png')
 root = tk.Tk()
 BgString = '#036597'
-canvas = tk.Canvas(root, height=300, width=300, bg=BgString)
+canvas = tk.Canvas(root, height=300, width=400, bg=BgString)
 canvas.grid(rowspan=3, columnspan=3)
+
+led = LED(18)
+dis = DistanceSensor(echo=15, trigger=14)
 
 times = 0
 def button_push():
@@ -15,10 +18,22 @@ def button_push():
 
 def hi():
     welcome_txt.config(text='Alarm System Is On')
+    dis.when_in_range = ledOn
+    dis.when_out_of_range = ledOff
     start_btn.config(command=lambda:bye(), text='Turn Off')
+
+def ledOn():
+    led.on()
+
+def ledOff():
+    led.off()
+
+def noth():
+    pass
 
 def bye():
     welcome_txt.config(text='Alarm System Is Off')
+    dis.when_in_range = noth
     start_btn.config(command=lambda:hi(), text='Turn On')
 
 welcome_txt = tk.Label(root, text='Welcome',
@@ -37,6 +52,4 @@ start_btn = tk.Button(root, text = 'Turn On',
 start_btn.grid(row=2, column=1)
 
 root.mainloop()
-
-
 

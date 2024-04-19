7cdb9daf20226d0b5698ef734878a7b45150de1e
MinerUNLEASHED <2020tray@gmail.com>
2024-04-19 11:08:00 -0500

    new project - CoolRGBLighting.py!

---
+++ RandomRGB/CoolRGBLighting.py
@@ -0,0 +1,15 @@
+from picozero import RGBLED
+from time import sleep
+from random import randint
+
+rgb = RGBLED(red = 1, green = 2, blue = 3)
+rgb.on()
+
+
+def newRand():
+    return randint(0,255)
+
+
+while True:
+    rgb.color = (newRand(), newRand(), newRand())
+    sleep(0.1)

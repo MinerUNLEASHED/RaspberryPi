a3cd40db78562cb11b321d4758bf0c74a8a6b765
MinerUNLEASHED <2020tray@gmail.com>
2024-04-27 12:17:27 -0500

    working on COL2024

--- COL 2024 Project/DistanceTest.py
+++ COL 2024 Project/DistanceTest.py
@@ -3,6 +3,7 @@ from time import sleep
 from signal import pause
 
 button = Button(15)
+button2 = Button(13)
 Redled = LED(2)
 Greenled = LED(4)
 
@@ -14,8 +15,18 @@ def rel():
     Redled.off()
     Greenled.on()
 
+def boP():
+    Redled.on()
+    Greenled.on()
+
+def boR():
+    Redled.off()
+    Greenled.off()
+
 button.when_pressed = press
 button.when_released = rel
+button2.when_pressed = boP
+button2.when_released = boR
 
 pause()
 

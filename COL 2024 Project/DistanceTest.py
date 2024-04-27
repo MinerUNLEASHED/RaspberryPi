f7cab2e9fdf0c51341949f738721223df7dae613
MinerUNLEASHED <2020tray@gmail.com>
2024-04-27 11:29:46 -0500

    working on COL2024

---
+++ COL 2024 Project/DistanceTest.py
@@ -0,0 +1,21 @@
+from gpiozero import LED, Button
+from time import sleep
+
+button = Button(23)
+Redled = LED(2)
+Greenled = LED(4)
+
+def press():
+    Redled.on()
+    Greenled.off()
+
+def rel():
+    Redled.off()
+    Greenled.on()
+
+button.when_pressed = press
+button.when_released = rel
+
+
+
+

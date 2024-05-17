3b0fce592cf4346a567c7c5d132c5dc1a99957c6
MinerUNLEASHED <2020tray@gmail.com>
2024-05-17 17:59:37 -0500

    working on COL2024

--- COL 2024 Project/PREVTrueFalseGame.py
+++ COL 2024 Project/PREVTrueFalseGame.py
@@ -1,19 +1,17 @@
-import random
 import tkinter as tk
-
-from gpiozero import LED, Button
-
+import random
+from gpiozero import DistanceSensor, LED, Button
+import time
 root = tk.Tk()
 BgString = '#036597'
 canvas = tk.Canvas(root, height=500, width=750, bg=BgString)
 canvas.grid(rowspan=3, columnspan=3)
 
+
 true = Button(15)
 false = Button(13)
 Redled = LED(2)
 Greenled = LED(4)
-
-
 # dis = DistanceSensor(echo=15, trigger=14, threshold_distance=0.9)
 
 # times = 0
@@ -30,23 +28,19 @@ def hi():
     # start_btn.config(command=lambda:bye(), text='Turn Off')
     welcome_txt.config(text="This Works!")
 
-
 def startGame():
     start_btn.grid_remove()
     author_txt.grid_remove()
-    welcome_txt.config(text="Background of COL Project:", font=("Railway", 23))
+    welcome_txt.config(text="Background of COL Project:", font=("Railway",23))
     root.after(1500, show_second_message)
-
 
 def show_second_message():
     welcome_txt.config(text="You will be asked five questions\nabout Al-Andalus")
     root.after(3000, show_third_message)
 
-
 def show_third_message():
     welcome_txt.config(text="Otherwise known as, Muslim Spain")
     root.after(3000, continueGame1)
-
 
 global ct
 ct = 0
@@ -62,11 +56,15 @@ def continueGame1():
     questions = allQA[0]
     answers = allQA[1]
     # print(allQA)
-    ct += 1
-    if (ct <= 5):
+    ct+=1
+    if(ct<=5):
         playQ(questions[0], answers[0])
     else:
         ending()
+
+
+
+
 
 
 def pick5():
@@ -94,32 +92,29 @@ def pick5():
 
     return result
 
-
 def playQ(question, answer):
     welcome_txt.config(text=question, wraplength=500)
     author_txt.config(text="Select Left For True And Right For False")
     author_txt.grid(row=0, column=1)
-    true.when_pressed = lambda: checkAnswer(1, answer)
-    false.when_pressed = lambda: checkAnswer(2, answer)
-
+    true.when_pressed = lambda:checkAnswer(1,answer)
+    false.when_pressed = lambda:checkAnswer(2,answer)
 
 def checkAnswer(guess, Ans):
     # true.close()
-    # false.close()TrueFalseGame.py
-    if guess == int(Ans):
+    # false.close()
+    if guess==int(Ans):
         Greenled.on()
         welcome_txt.config(text="Correct!")
     else:
         Redled.on()
-        config = welcome_txt.config(text="Incorrect!")
-    root.after(3000, resetLEDs)
+        welcome_txt.config(text="Incorrect!")
+    root.after(3000,resetLEDs)
 
 
 def resetLEDs():
     Greenled.off()
     Redled.off()
     continueGame1()
-
 
 # def ledOn():
 #     led.on()
@@ -136,11 +131,16 @@ def resetLEDs():
 #     # start_btn.config(command=lambda:hi(), text='Turn On')
 #     welcome_txt.config(text="it works")
 
-author_txt = tk.Label(root, text="Aalim, Aalam, Aliyan, And Azhmeer 2024", font=("Raleway", 19), bg=BgString,
+author_txt = tk.Label(root, text="Aalim, Aalam, Aliyan, And Azhmeer 2024",
+                      font=("Raleway", 19),
+                      bg=BgString,
                       fg="#ffb703")
 author_txt.grid(row=0, column=1)
 
-welcome_txt = tk.Label(root, text='COL True And False Game', font=('Raleway', 25), bg=BgString, fg='#ffb703')
+welcome_txt = tk.Label(root, text='COL True And False Game',
+                       font=('Raleway', 25),
+                       bg=BgString,
+                       fg='#ffb703')
 welcome_txt.grid(row=1, column=1)
 
 # score_txt = tk.Label(root, text="",
@@ -149,8 +149,14 @@ welcome_txt.grid(row=1, column=1)
 #                      fg="#ffb703")
 # score_txt.grid(row=2, column=2)
 
-start_btn = tk.Button(root, text='Start Playing', height=2, width=15, command=lambda: startGame(), bg='#ffb703',
-                      activebackground='#fb8500', fg='#023047', activeforeground='#FFFFFF')
+start_btn = tk.Button(root, text = 'Start Playing',
+                      height=2, width=15,
+                      command=lambda:startGame(),
+                      bg='#ffb703',
+                      activebackground='#fb8500',
+                      fg='#023047',
+                      activeforeground='#FFFFFF')
 start_btn.grid(row=2, column=1)
 
 root.mainloop()
+
